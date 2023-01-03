3d0a42b4b6822416900a8c5a34a24e5e475e0eee
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-01-03 20:06:47 +0800

    feat: 🎸 get tab with browser polyfill

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,16 @@
+import Browser from "webextension-polyfill";
+
 export const test = () => {
   console.log("background");
 };
 test();
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await Browser.tabs.query(queryOptions);
+  console.log("tab", tab);
+  return tab;
+}
+
+getCurrentTab();
