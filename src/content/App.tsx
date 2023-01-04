097e1c463c6f4bae4522f09f9e99d82a2cd06f9b
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-01-04 12:33:05 +0800

    feat: 🎸 init content

---
+++ src/content/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        height: "100px",
+        width: "100px",
+        backgroundColor: "orange",
+        position: "absolute",
+        top: "0px",
+        right: "0px",
+        zIndex: "99999",
+      }}
+    >
+      App
+    </div>
+  );
+}
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.append(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
