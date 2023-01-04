57f8a227d184ddbe8c4c5667ed798d7c9b571d19
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-01-04 11:49:43 +0800

    feat: 🎸 test tab events

--- src/background.ts
+++ src/background.ts
@@ -16,8 +16,99 @@ async function getCurrentTab() {
 getCurrentTab();
 
 async function getAllTabs() {
-  let [tab] = await Browser.tabs.query({ active: false });
+  let tab = await Browser.tabs.query({ currentWindow: true });
   console.log("allTabs", tab);
   return tab;
 }
 getAllTabs();
+
+const handleCreateTab = async () => {
+  Browser.tabs.onCreated.addListener((res) => {
+    console.log("tab created", res);
+  });
+};
+handleCreateTab();
+
+const handleCloseTab = async () => {
+  Browser.tabs.onRemoved.addListener((res) => {
+    console.log("tab removed", res);
+  });
+};
+handleCloseTab();
+
+const handleDetachTab = async () => {
+  Browser.tabs.onDetached.addListener((res) => {
+    console.log("tab detached", res);
+  });
+};
+handleDetachTab();
+
+const handleActivetTab = async () => {
+  Browser.tabs.onActivated.addListener((res) => {
+    console.log("tab onActivated", res);
+  });
+};
+handleActivetTab();
+
+// const handleonActiveChanged = async () => {
+//   Browser.tabs?.onActiveChanged.addListener((res) => {
+//     console.log("tab onActiveChanged", res);
+//   });
+// };
+// handleonActiveChanged();
+
+const handleAttached = async () => {
+  Browser.tabs.onAttached.addListener((res) => {
+    console.log("tab onAttached", res);
+  });
+};
+handleAttached();
+
+// const handleonHighlightChanged = async () => {
+//   Browser.tabs?.onHighlightChanged.addListener((res) => {
+//     console.log("tab onAttaconHighlightChangedhed", res);
+//   });
+// };
+// handleonHighlightChanged();
+
+const handleonHighlightedTab = async () => {
+  Browser.tabs.onHighlighted.addListener((res) => {
+    console.log("tab onHighlighted", res);
+  });
+};
+handleonHighlightedTab();
+
+const handleonMovedTab = async () => {
+  Browser.tabs.onMoved.addListener((res) => {
+    console.log("tab onMoved", res);
+  });
+};
+handleonMovedTab();
+
+const handleonReplacedTab = async () => {
+  Browser.tabs.onReplaced.addListener((res) => {
+    console.log("tab onReplaced", res);
+  });
+};
+handleonReplacedTab();
+
+// const handleonSelectionChangedTab = async () => {
+//   Browser.tabs.onSelectionChanged.addListener((res) => {
+//     console.log("tab onSelectionChanged", res);
+//   });
+// };
+// handleonSelectionChangedTab();
+
+const handleonUpdatedTab = async () => {
+  Browser.tabs.onUpdated.addListener((res) => {
+    console.log("tab onUpdated", res);
+  });
+};
+handleonUpdatedTab();
+
+const handleonZoomChangeTab = async () => {
+  Browser.tabs.onZoomChange.addListener((res) => {
+    console.log("tab onZoomChange", res);
+  });
+};
+handleonZoomChangeTab();
